7d45ae9eeaf57512e8f91745d73a58a7d612395c
cpc26 <cpc26@mac.com>
2014-06-16 13:56:52 -0400

    init the repo with $HOME bin and src

---
+++ src/abcl/build/classes/org/armedbear/lisp/system.lisp
@@ -0,0 +1,6 @@
+
+(SETF (LOGICAL-PATHNAME-TRANSLATIONS "sys")
+      '(("SYS:SRC;**;*.*"
+         "/Users/cpc26/src/abcl/src/org/armedbear/lisp/**/*.*")
+        ("SYS:JAVA;**;*.*"
+         "/Users/cpc26/src/abcl/src/org/armedbear/lisp/../../../**/*.*")))
